50641141c97a19504b683ce37882358b91adc306
Tiago Matias <timatias@c3r2s1.42luanda.com>
2025-06-18 13:46:52 +0100

    d

--- server/vite.ts
+++ server/vite.ts
@@ -22,8 +22,8 @@ export function log(message: string, source = "express") {
 export async function setupVite(app: Express, server: Server) {
   const serverOptions = {
     middlewareMode: true,
-    hmr: {server },
-    allowedHosts: true,
+    hmr: { server },
+    allowedHosts: ['0.0.0.0', 'localhost'],
   };
 
   const vite = await createViteServer({
